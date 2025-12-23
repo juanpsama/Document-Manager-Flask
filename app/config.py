23f54242905426b8ea2f57d5d18310e5de2302f7
juanpsama <samajpablo@gmail.com>
2025-12-23 09:57:00 -0600

    feat: update the config

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-SECRET_KEY = os.environ.get('FLASK_KEY')
+SECRET_KEY = os.environ.get('FLASK_KEY', 'super-secret-key')
 DB_URL = os.environ.get("DB_URI", "sqlite:///posts.db")
 UPLOAD_DESTINATION = 'files/'
 
